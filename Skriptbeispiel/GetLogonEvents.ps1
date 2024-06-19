c3f6455bfbdbf45aefe69a5d43366f4f131b70d8
Stefan Ober <StefanO@ppedv.de>
2024-06-19 13:35:43 +0200

    Verbose Ausgabe

--- Skriptbeispiel/GetLogonEvents.ps1
+++ Skriptbeispiel/GetLogonEvents.ps1
@@ -6,5 +6,9 @@
 
 [string]$ComputerName = "localhost"
 )
+Write-Verbose -Message "ZusatzInfo:"
+Write-Verbose -Message "EventId: $EventId"
+Write-Verbose -Message "Newest: $Newest"
+Write-Verbose -Message "Computername: $ComputerName"
 
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventID -eq $EventId | Select-Object -First $Newest
